7206c77bfe97fdb4593b25f2c8014c5e837b5acb
Ryan Harris <thomasryanharris@gmail.com>
2025-04-12 20:40:09 -0700

    Wrap up translation of fluid sim

--- pkg/fluid/fluid.go
+++ pkg/fluid/fluid.go
@@ -1,13 +1,24 @@
 package fluid
 
+import "math"
+
+var (
+	Relaxation float32 = 1.9
+)
+
 type Fluid struct {
 	density float32
 	h       float32
 
 	numX, numY int
+	numCells   int
 	u, v       []float32 // velocities
+	newU, newV []float32
 	p          []float32
-	s          []int // solid (0) or liquid (1)
+	s          []float32 // solid (0) or liquid (1)
+
+	m    []float32 // smoke
+	newM []float32
 }
 
 func New(density float32, width, height int, h float32) *Fluid {
@@ -16,12 +27,17 @@ func New(density float32, width, height int, h float32) *Fluid {
 		density: density,
 		h:       h,
 
-		numX: width + 2,
-		numY: height + 2,
-		u:    make([]float32, 0, numCells),
-		v:    make([]float32, 0, numCells),
-		p:    make([]float32, 0, numCells),
-		s:    make([]int, 0, numCells),
+		numX:     width + 2,  // LR border cells
+		numY:     height + 2, // TB border cells
+		numCells: numCells,
+		u:        make([]float32, 0, numCells),
+		v:        make([]float32, 0, numCells),
+		newU:     make([]float32, 0, numCells),
+		newV:     make([]float32, 0, numCells),
+		p:        make([]float32, 0, numCells),
+		s:        make([]float32, 0, numCells),
+		m:        make([]float32, 0, numCells),
+		newM:     make([]float32, 0, numCells),
 	}
 }
 
@@ -31,6 +47,9 @@ func (f *Fluid) Simulate(dt, gravity float32, numIters uint) {
 		f.p[i] = 0
 	}
 	f.makeIncompressible(numIters, dt)
+	f.handleBorders()
+	f.advectVelocity(dt)
+	f.advectSmoke(dt)
 }
 
 func (f *Fluid) handleGravity(dt, gravity float32) {
@@ -46,28 +65,197 @@ func (f *Fluid) handleGravity(dt, gravity float32) {
 }
 
 func (f *Fluid) makeIncompressible(numIters uint, dt float32) {
-	n := f.numY
-	cp := f.density * f.h / dt
-	_ = cp
-	for range numIters {
+	numY := f.numY
+	cp := f.density * float32(f.h) / dt
 
+	for range numIters {
 		for i := 1; i < f.numX-1; i++ {
 			for j := 1; j < f.numY-1; j++ {
 				// If the cell is solid, nothing to do...
-				if f.s[i*n+j] == 0 {
+				if f.s[i*numY+j] == 0 {
 					continue
 				}
 
-				sx0 := f.s[(i-1)*n+j]
-				sx1 := f.s[(i+1)*n+j]
-				sy0 := f.s[i*n+j-1]
-				sy1 := f.s[i*n+j+1]
+				sx0 := f.s[(i-1)*numY+j]
+				sx1 := f.s[(i+1)*numY+j]
+				sy0 := f.s[i*numY+j-1]
+				sy1 := f.s[i*numY+j+1]
 				s := sx0 + sx1 + sy0 + sy1
-				if s == 0 {
+				if s == 0 { // All adjacent cells are solid, nothing to do...
 					continue
 				}
-				// TODO: Pickup here
+
+				div := f.u[(i+1)*numY+j] - f.u[i*numY+j] +
+					f.v[i*numY+j+1] - f.v[i*numY+j]
+
+				p := -div / s
+				p *= Relaxation
+				f.p[i*numY+j] += cp * p
+
+				f.u[i*numY+j] -= sx0 * p
+				f.u[(i+1)*numY+j] += sx1 * p
+				f.v[i*numY+j] -= sy0 * p
+				f.v[i*numY+j+1] += sy1 * p
 			}
 		}
+	}
+}
+
+func (f *Fluid) handleBorders() {
+	numY := f.numY
+	for i := range f.numX {
+		f.u[i*numY+0] = f.u[i*numY+1]         // top border
+		f.u[(i+1)*numY-1] = f.u[(i+1)*numY-2] // bottom border
+	}
+
+	numX := f.numX
+	for j := range f.numY {
+		f.v[0*numY+j] = f.v[1*numY+j]               // left border
+		f.v[(numX-1)*numY+j] = f.v[(numX-2)*numY+j] // right border
+	}
+}
+
+func (f *Fluid) advectVelocity(dt float32) {
+	// TODO: Remove f clearing of the array?
+	for i := range f.numX {
+		f.newU[i] = f.u[i]
+	}
+	for j := range f.numY {
+		f.newV[j] = f.v[j]
+	}
+
+	n := f.numY
+	h := f.h
+	h2 := h / 2
+
+	for i := 1; i < f.numX-1; i++ {
+		for j := 1; j < f.numY-1; j++ {
+
+			// u component
+			if f.s[i*n+j] != 0.0 && f.s[(i-1)*n+j] != 0.0 {
+				x := float32(i) * h
+				y := float32(j)*h + h2
+				u := f.u[i*n+j]
+				v := f.avgV(i, j)
+
+				x = x - dt*u
+				y = y - dt*v
+				u = f.sampleField(x, y, fieldU)
+				f.newU[i*n+j] = u
+			}
+
+			// v component
+			if f.s[i*n+j] != 0.0 && f.s[i*n+j-1] != 0.0 {
+				x := float32(i)*h + h2
+				y := float32(j) * h
+				u := f.avgU(i, j)
+				v := f.v[i*n+j]
+
+				x = x - dt*u
+				y = y - dt*v
+				v = f.sampleField(x, y, fieldV)
+				f.newV[i*n+j] = v
+			}
+		}
+	}
+
+	for i := range f.numX {
+		f.u[i] = f.newU[i]
+	}
+	for j := range f.numY {
+		f.v[j] = f.newV[j]
+	}
+}
+
+func (f *Fluid) avgV(i, j int) float32 {
+	numY := f.numY
+	v := (f.v[(i-1)*numY+j] + f.v[i*numY+j] +
+		f.v[(i-1)*numY+j+1] + f.v[i*numY+j+1]) * 0.25
+	return v
+}
+
+func (f *Fluid) avgU(i, j int) float32 {
+	n := f.numY
+	u := (f.u[i*n+j-1] + f.u[i*n+j] +
+		f.u[(i+1)*n+j-1] + f.u[(i+1)*n+j]) * 0.25
+	return u
+}
+
+type field int
+
+const (
+	fieldU field = iota
+	fieldV
+	fieldS
+)
+
+func (f *Fluid) sampleField(x, y float32, fld field) float32 {
+	n := f.numY
+	h := float32(f.h)
+	h1 := float32(1.0 / float32(h))
+	h2 := float32(0.5 * float32(h))
+
+	x = max(min(x, float32(f.numX)*h), float32(h))
+	y = max(min(y, float32(f.numY)*h), float32(h))
+
+	dx, dy := float32(0.0), float32(0.0)
+
+	var fieldToSample []float32
+	switch fld {
+	case fieldU:
+		fieldToSample = f.u
+		dy = h2
+	case fieldV:
+		fieldToSample = f.v
+		dx = h2
+	case fieldS:
+		fieldToSample = f.s
+		dx, dy = h2, h2
+	}
+
+	x0 := min(int(math.Floor(float64((x-dx)*h1))), f.numX-1)
+	tx := ((x - dx) - float32(x0)*h) * h1
+	x1 := min(x0+1, f.numX-1)
+
+	y0 := min(int(math.Floor(float64((y-dy)*h1))), f.numY-1)
+	ty := ((y - dy) - float32(y0)*h) * h1
+	y1 := min(y0+1, f.numY-1)
+
+	sx := 1.0 - tx
+	sy := 1.0 - ty
+
+	val := sx*sy*fieldToSample[x0*n+y0] +
+		tx*sy*fieldToSample[x1*n+y0] +
+		tx*ty*fieldToSample[x1*n+y1] +
+		sx*ty*fieldToSample[x0*n+y1]
+
+	return val
+}
+
+func (f *Fluid) advectSmoke(dt float32) {
+	for i := range f.numCells {
+		f.newM[i] = f.m[i]
+	}
+
+	n := f.numY
+	h := float32(f.h)
+	h2 := 0.5 * float32(h)
+
+	for i := 1; i < f.numX-1; i++ {
+		for j := 1; j < f.numY-1; j++ {
+
+			if f.s[i*n+j] != 0.0 {
+				var u = (f.u[i*n+j] + f.u[(i+1)*n+j]) * 0.5
+				var v = (f.v[i*n+j] + f.v[i*n+j+1]) * 0.5
+				var x = float32(i)*h + h2 - dt*u
+				var y = float32(j)*h + h2 - dt*v
+
+				f.newM[i*n+j] = f.sampleField(x, y, fieldS)
+			}
+		}
+	}
+
+	for i := range f.numCells {
+		f.m[i] = f.newM[i]
 	}
 }
