4bfe39110c21ebe86a6ab0594b9f4673f9827885
Ryan Harris <thomasryanharris@gmail.com>
2025-04-13 17:19:52 -0700

    Start displaying sampling from  data

--- main/main.go
+++ main/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"log"
-	"math/rand/v2"
+	"math"
 
 	"github.com/TheFellow/fluid/pkg/fluid"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -19,39 +18,66 @@ const (
 )
 
 type Game struct {
-	counter int
-	bgColor color.RGBA
-
 	fluid *fluid.Fluid
 }
 
 func NewGame() *Game {
-	f := fluid.New(1000, fluidWidth, fluidHeight, 1000)
+	f := fluid.New(1000, fluidWidth, fluidHeight, float32(screenHeight)/200.0)
+	// Set no walls except floor
+	for i := range fluidWidth {
+		for j := range fluidHeight {
+			if i == 0 || i == fluidWidth-1 || j == 0 || j == fluidHeight-1 {
+				f.SetSolid(i, j, true)
+			} else {
+				f.SetSolid(i, j, false)
+			}
+		}
+	}
 	return &Game{
 		fluid: f,
 	}
 }
 
 func (g *Game) Update() error {
-	g.counter++
-	if g.counter%ebiten.TPS() == 0 {
-		g.bgColor = color.RGBA{
-			R: uint8(rand.IntN(256)),
-			G: uint8(rand.IntN(256)),
-			B: uint8(rand.IntN(256)),
-			A: 0xff,
-		}
-	}
-
-	dt := float32(1.0 / ebiten.ActualTPS())
+	dt := float32(1.0 / float32(ebiten.TPS()))
 	g.fluid.Simulate(dt, -9.81, 10)
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(g.bgColor)
+	minPressure := float32(math.MaxFloat32)
+	maxPressure := float32(0)
+	pressures := g.fluid.Pressure()
+	for i := range fluidWidth {
+		for j := range fluidHeight {
+			p, err := pressures.Value(i, j)
+			if err != nil {
+				log.Panicf("cannot get pressure: %v", err)
+			}
+			if p < minPressure {
+				minPressure = p
+			}
+			if p > maxPressure {
+				maxPressure = p
+			}
+		}
+	}
+	for i := range fluidWidth {
+		for j := range fluidHeight {
+			_, _ = i, j
+		}
+	}
 
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FluidSim - FPS: %0.2f", ebiten.ActualFPS()))
+	v := g.fluid.Velocity()
+	x, y, err := v.Value(fluidWidth/2, fluidHeight/2)
+	if err != nil {
+		log.Panicf("cannot get velocity: %v", err)
+	}
+
+	ebitenutil.DebugPrint(screen,
+		fmt.Sprintf("FluidSim - FPS: %0.2f\nPressures: [%0.2f, %0.2f]\nVelocity: (%0.2f, %0.2f)",
+			ebiten.ActualFPS(), minPressure, maxPressure, x, y),
+	)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (w, hHeight int) {
