0b60c85d4802fafec4164ea374b8e77aed3699a0
Ryan Harris <ThomasRyanHarris@gmail.com>
2025-06-10 21:20:05 -0700

    Improve rendering performance

--- pkg/fluid/pressure.go
+++ pkg/fluid/pressure.go
@@ -5,9 +5,7 @@ import "math"
 func (f *Fluid) Pressure() ScalarField {
 	minValue := float32(math.MaxFloat32)
 	maxValue := float32(-(math.MaxFloat32 - 1))
-	pCopy := make([]float32, f.numCells)
 	for i := range f.numCells {
-		pCopy[i] = f.p[i]
 		if f.p[i] < minValue {
 			minValue = f.p[i]
 		}
--- pkg/fluid/smoke.go
+++ pkg/fluid/smoke.go
@@ -5,9 +5,7 @@ import "math"
 func (f *Fluid) Smoke() ScalarField {
 	minValue := float32(math.MaxFloat32)
 	maxValue := float32(-(math.MaxFloat32 - 1))
-	mCopy := make([]float32, f.numCells)
 	for i := range f.numCells {
-		mCopy[i] = f.m[i]
 		if f.m[i] < minValue {
 			minValue = f.m[i]
 		}
